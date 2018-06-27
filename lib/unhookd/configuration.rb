9036f6730296fc33bc6289310b7980f407f2e267
Jason Carter <jason@mavenlink.com>
2018-06-26 20:35:42 -0600

    Allow repo_name and chart_name to be configured.

--- lib/unhookd/configuration.rb
+++ lib/unhookd/configuration.rb
@@ -1,9 +1,11 @@
 module Unhookd
   class Configuration
-    attr_accessor :unhookd_url
+    attr_accessor :unhookd_url, :repo_name, :chart_name
 
     def initialize
-      @unhookd_url = 'http://localhost:8080'
+      @unhookd_url = 'http://localhost:8080' # The url that Unhookd exposes
+      @repo_name   = 'repo_name'             # The Git repository name of the project
+      @chart_name  = 'repo/chart'            # The Charts repository and name for Unhookd to deploy
     end
   end
 end
--- spec/unhookd/configuration_spec.rb
+++ spec/unhookd/configuration_spec.rb
@@ -3,17 +3,47 @@ require "spec_helper"
 RSpec.describe Unhookd::Configuration do
   context "default values" do
     describe "#unhookd_url" do
-      it "default value is 'http://localhost:8080'" do
+      it "has a default value of 'http://localhost:8080'" do
         expect(Unhookd::Configuration.new.unhookd_url).to eq("http://localhost:8080")
+      end
+    end
+
+    describe "#chart_name" do
+      it "has a default value of 'repo/chart'" do
+        expect(Unhookd::Configuration.new.chart_name).to eq("repo/chart")
+      end
+    end
+
+    describe "#repo_name" do
+      it "has a default value of 'repo_name'" do
+        expect(Unhookd::Configuration.new.repo_name).to eq("repo_name")
       end
     end
   end
 
-  describe "#unhookd_url=" do
-    it "can set value" do
-      config = Unhookd::Configuration.new
-      config.unhookd_url = "https://some-in-cluster-url.com"
-      expect(config.unhookd_url).to eq("https://some-in-cluster-url.com")
+  describe "setters" do
+    describe "#unhookd_url=" do
+      it "can set a value" do
+        config = Unhookd::Configuration.new
+        config.unhookd_url = "https://some-in-cluster-url.com"
+        expect(config.unhookd_url).to eq("https://some-in-cluster-url.com")
+      end
+    end
+
+    describe "#chart_name=" do
+      it "can set a value" do
+        config = Unhookd::Configuration.new
+        config.chart_name = "somechart/name"
+        expect(config.chart_name).to eq("somechart/name")
+      end
+    end
+
+    describe "#repo_name=" do
+      it "can set a value" do
+        config = Unhookd::Configuration.new
+        config.repo_name = "some_repo"
+        expect(config.repo_name).to eq("some_repo")
+      end
     end
   end
 end
