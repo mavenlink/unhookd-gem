5d504bb09f0e2baa7a259cd21a2ec34ccc9f7cf4
Jason Carter <jason@mavenlink.com>
2018-06-26 20:46:30 -0600

    Send a request to the configured Unhookd url with the correct values when Unhookd::Deployer#deploy! is called.

--- lib/unhookd/deployer.rb
+++ lib/unhookd/deployer.rb
@@ -1,9 +1,32 @@
 module Unhookd
   class Deployer
     def initialize(sha, branch)
+      @branch = branch
+      @sha = sha
+      @final_values = {}
+      @config = Unhookd.configuration
     end
 
     def deploy!
+      HTTParty.post(
+        @config.unhookd_url,
+        body: get_values.to_json,
+        headers: { "Content-Type" => "application/json" },
+        verify: false,
+      )
+    end
+
+    private
+
+    def get_values
+      @final_values.merge!(
+        {
+          "branch" => @branch,
+          "release" => @branch,
+          "repo" => @config.repo_name,
+          "chart" => @config.chart_name,
+        }
+      )
     end
   end
 end
--- spec/unhookd/deployer_spec.rb
+++ spec/unhookd/deployer_spec.rb
@@ -4,9 +4,25 @@ RSpec.describe Unhookd::Deployer do
   describe "#deploy" do
     let(:sha) { "123" }
     let(:branch) { "my-branch" }
+    let(:expected_url) { Unhookd.configuration.unhookd_url}
+    let(:expected_body) do
+      {
+        "branch" => branch,
+        "release" => branch,
+        "repo" => Unhookd.configuration.repo_name,
+        "chart" => Unhookd.configuration.chart_name,
+      }.to_json
+    end
+    let(:expected_headers) { { "Content-Type" => "application/json" } }
 
-    it "exists" do
-      expect { described_class.new(sha, branch).deploy! }.to_not raise_error
+    subject { described_class.new(sha, branch) }
+
+    it "sends a request to the configured endpoint with the correct values" do
+      expect(HTTParty)
+        .to receive(:post)
+        .with(expected_url, body: expected_body, headers: expected_headers, verify: false)
+
+      subject.deploy!
     end
   end
 end
