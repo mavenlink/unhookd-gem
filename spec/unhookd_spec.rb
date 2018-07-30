5de11be0d49e0b6f6750d3273843c51a6721393d
Jason Carter <jason@mavenlink.com>
2018-07-30 14:42:42 -0700

    Validate that required params are present
    Signed-off-by: Wesley Morlock <wmorlock@mavenlink.com>

--- spec/unhookd_spec.rb
+++ spec/unhookd_spec.rb
@@ -40,11 +40,35 @@ RSpec.describe Unhookd do
     let(:chart_values) { { foo: 'bar' }}
     let(:deployer_stub) { Unhookd::Deployer.new(release_name, chart_values) }
 
-    it "initializes an Unhookd::Deployer with passed args and calls #deploy! on it" do
-      expect(Unhookd::Deployer).to receive(:new).with(release_name, chart_values).and_return(deployer_stub)
-      expect(deployer_stub).to receive(:deploy!)
 
-      Unhookd.deploy!(release_name, chart_values)
+    context "if required configuration values are present" do
+      before do
+        Unhookd.configure do |config|
+          config.unhookd_url = 'www.some-url-in-a-cluster.com'
+          config.chart_name = 'some/repo'
+        end
+      end
+
+      it "initializes an Unhookd::Deployer with passed args and calls #deploy! on it" do
+        expect(Unhookd::Deployer).to receive(:new).with(release_name, chart_values).and_return(deployer_stub)
+        expect(deployer_stub).to receive(:deploy!)
+
+        Unhookd.deploy!(release_name, chart_values)
+      end
+    end
+
+    context "if required configuration values are not present" do
+      before do
+        expect(Unhookd.configuration.unhookd_url).to be_nil
+        expect(Unhookd.configuration.chart_name).to be_nil
+      end
+
+      it "raises the Unhookd missing args error" do
+        expect {
+          Unhookd.deploy!(release_name, chart_values)
+        }.to raise_error(Unhookd::InvalidConfiguration)
+
+      end
     end
   end
 end
